e92eb6b437dea9c488b18838e02265ded08f3a8a
Gl00mySunrise <ms.unknown.k@gmail.com>
2020-04-01 19:58:49 +0300

    Hidden secret key

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cg=w*l!(t*#2v07xlwisqdt4*#%(eja5%29kua00c=q0$q%(i@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
