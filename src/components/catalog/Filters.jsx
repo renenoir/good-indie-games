bd9e16e8e102e80167df87fb2406a51ec4883b07
Dmitry Boltachev <dmitryboltachev@gmail.com>
2020-05-24 17:12:45 +0300

    Filters mnodal is rewritten using react-responsive-modal

--- src/components/catalog/Filters.jsx
+++ src/components/catalog/Filters.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import TextField from "@atlaskit/textfield";
 import { CheckboxSelect } from "@atlaskit/select";
 import Button from "@atlaskit/button";
-import ModalDialog, { ModalTransition } from "@atlaskit/modal-dialog";
+import { Modal } from "react-responsive-modal";
+import "react-responsive-modal/styles.css";
 
 import useFetchFilter from "../hooks/useFetchFilter";
 
@@ -38,92 +39,114 @@ function Filters({
       <Button appearance="primary" onClick={onOpen}>
         Filters
       </Button>
-      <ModalTransition>
-        {isOpen && (
-          <ModalDialog scrollBehavior="outside" onClose={onClose}>
-            <Form method="POST">
-              <h1>Filters</h1>
-              <Fields>
-                <FieldWrap>
-                  <Label htmlFor="year-from">Year from</Label>
-                  <TextField
-                    id="year-from"
-                    name="year-from"
-                    placeholder="2000"
-                    maxLength={4}
-                    value={dateGte}
-                    onChange={(e) => setDateGte(e.target.value)}
-                  />
-                </FieldWrap>
-                <FieldWrap>
-                  <Label htmlFor="year-to">Year to</Label>
-                  <TextField
-                    id="year-to"
-                    name="year-to"
-                    placeholder="2020"
-                    maxLength={4}
-                    value={dateLte}
-                    onChange={(e) => setDateLte(e.target.value)}
-                  />
-                </FieldWrap>
-                <FieldWrap>
-                  <Label htmlFor="genres">Genres</Label>
-                  <CheckboxSelect
-                    id="genres"
-                    name="genres"
-                    options={genres}
-                    value={selectedGenres}
-                    onChange={setSelectedGenres}
-                  />
-                </FieldWrap>
-                <FieldWrap>
-                  <Label htmlFor="themes">Themes</Label>
-                  <CheckboxSelect
-                    id="themes"
-                    name="themes"
-                    options={themes}
-                    value={selectedThemes}
-                    onChange={setSelectedThemes}
-                  />
-                </FieldWrap>
-                <FieldWrap>
-                  <Label htmlFor="platforms">Platforms</Label>
-                  <CheckboxSelect
-                    id="platforms"
-                    name="platforms"
-                    options={platforms}
-                    value={selectedPlatforms}
-                    onChange={setSelectedPlatforms}
-                  />
-                </FieldWrap>
-              </Fields>
-            </Form>
-          </ModalDialog>
-        )}
-      </ModalTransition>
+      <Modal
+        open={isOpen}
+        onClose={onClose}
+        center
+        classNames={{
+          modal: "filtersModal",
+        }}
+      >
+        <Form method="POST">
+          <Header>Filters</Header>
+          <Fields>
+            <FieldWrap>
+              <Label htmlFor="year-from">Year from</Label>
+              <TextField
+                id="year-from"
+                name="year-from"
+                placeholder="2000"
+                maxLength={4}
+                value={dateGte}
+                onChange={(e) => setDateGte(e.target.value)}
+              />
+            </FieldWrap>
+            <FieldWrap>
+              <Label htmlFor="year-to">Year to</Label>
+              <TextField
+                id="year-to"
+                name="year-to"
+                placeholder="2020"
+                maxLength={4}
+                value={dateLte}
+                onChange={(e) => setDateLte(e.target.value)}
+              />
+            </FieldWrap>
+            <FieldWrap>
+              <Label htmlFor="genres">Genres</Label>
+              <CheckboxSelect
+                id="genres"
+                name="genres"
+                options={genres}
+                value={selectedGenres}
+                onChange={setSelectedGenres}
+              />
+            </FieldWrap>
+            <FieldWrap>
+              <Label htmlFor="themes">Themes</Label>
+              <CheckboxSelect
+                id="themes"
+                name="themes"
+                options={themes}
+                value={selectedThemes}
+                onChange={setSelectedThemes}
+              />
+            </FieldWrap>
+            <FieldWrap>
+              <Label htmlFor="platforms">Platforms</Label>
+              <CheckboxSelect
+                id="platforms"
+                name="platforms"
+                options={platforms}
+                value={selectedPlatforms}
+                onChange={setSelectedPlatforms}
+              />
+            </FieldWrap>
+          </Fields>
+        </Form>
+      </Modal>
+      <ModalStyles />
     </Wrapper>
   );
 }
+
+const ModalStyles = createGlobalStyle`
+  .filtersModal {
+    width: 500px;
+  }
+`;
 
 const Wrapper = styled.div`
   padding: 1rem 0 1.25rem;
 `;
 
 const Form = styled.form`
-  padding: 0.5rem 0 1.5rem;
+  padding: 0.5rem 0;
+`;
+
+const Header = styled.h2`
+  margin-top: 0;
 `;
 
 const Fields = styled.div`
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-  margin: 0 -1.25rem -1.25rem 0;
+  @media (min-width: 40rem) {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    margin: 0 -1.25rem -1.25rem 0;
+  }
 `;
 
 const FieldWrap = styled.div`
-  flex-basis: calc(50% - 1.25rem);
-  max-width: calc(50% - 1.25rem);
-  margin: 0 1.25rem 1.25rem 0;
+  &:not(:last-child) {
+    margin-bottom: 1.25rem;
+  }
+
+  @media (min-width: 40rem) {
+    flex-basis: calc(50% - 1.25rem);
+    max-width: calc(50% - 1.25rem);
+    margin: 0 1.25rem 1.25rem 0;
+  }
 `;
 
 const Label = styled.label``;
