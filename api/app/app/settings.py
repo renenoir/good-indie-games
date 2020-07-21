4195b5e0927a313c77b6862bbc80f6628cb3326e
Gl00mySunrise <ms.unknown.k@gmail.com>
2020-07-21 18:42:41 +0300

    Preparing to deploy

--- api/app/app/settings.py
+++ api/app/app/settings.py
@@ -131,7 +131,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
 
 AUTH_USER_MODEL = 'core.User'
 
