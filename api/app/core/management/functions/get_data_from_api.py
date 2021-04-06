d54811a082d8d89b525713a3bf5266f71e0470f8
renenoir <ms.unknown.k@gmail.com>
2021-04-06 13:54:34 +0300

    Migrated from igdb to twitch api

--- api/app/core/management/functions/get_data_from_api.py
+++ api/app/core/management/functions/get_data_from_api.py
@@ -1,15 +1,28 @@
 import requests
+import os
 
 
 def get_data_from_api(order, limit, offset):
     """Get data from igdb API"""
-    url = 'https://api-v3.igdb.com/games/'
+    token_url = 'https://id.twitch.tv/oauth2/token'
+    token_params = {
+        'client_id': os.environ.get('CLIENT_ID'),
+        'client_secret': os.environ.get('CLIENT_SECRET'),
+        'grant_type': 'client_credentials'
+    }
+    token_req = requests.post(token_url, params=token_params)
+    token = token_req.json().get('access_token')
 
-    headers = {'user-key': 'f58d91c59942416ed14564e116f79a41'}
+    url = 'https://api.igdb.com/v4/games'
+
+    headers = {
+        'Client-ID': os.environ.get('CLIENT_ID'),
+        'Authorization': 'Bearer ' + str(token)
+    }
 
     data = 'fields name, summary, websites.url, similar_games.name, \
             platforms.name, involved_companies.company.name, \
-            involved_companies.developer, cover.url, popularity, \
+            involved_companies.developer, cover.url,  \
             genres.name, themes.name, total_rating, first_release_date; \
             where total_rating > 65 & total_rating_count >= 8 & \
             genres = (32); sort created_at ' + f"{order}" + '; \
